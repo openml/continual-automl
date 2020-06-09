4ac2dab6169c1276d08925e4fa5e226b809ab7f6
bilgecelik <bilgecelik@gmail.com>
2020-06-09 18:11:32 +0200

    del2

--- Scripts/run3_3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-
-source activate myenv
-python3 runs_3_3.py
--- Scripts/run3_5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-
-source activate myenv
-python3 runs_3_5_cntd.py
